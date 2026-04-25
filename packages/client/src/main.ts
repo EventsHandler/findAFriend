faf0182988d0ca40353855db670abb77b382ee8e
cristichiu <grajdiancristian@gmail.com>
2026-04-25 16:11:02 +0300

    clean up

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -2,13 +2,26 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { apolloClient } from './apollo'
 
 const app = createApp(App)
 
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    ...routes,
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/teacher/list',
+    },
+  ],
+})
+
 app.use(router)
+
 app.provide(DefaultApolloClient, apolloClient)
 
 app.mount('#app')
