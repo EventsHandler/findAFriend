f3b6d6da2b367d155b30d7b6563bc26b08d1176c
cristichiu <grajdiancristian@gmail.com>
2026-04-26 15:18:02 +0300

    add vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -6,8 +6,8 @@ import VueRouter from 'unplugin-vue-router/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: process.env.BASE_PATH || '/',
   plugins: [
     VueRouter({
       routesFolder: 'src/views',
@@ -21,15 +21,30 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3000',
-      },
-      // Only proxy the API endpoint, not the SPA route `/chat/:name`
-      '/chat/send': {
-        target: 'http://localhost:3000',
-      },
-    },
-  },
-})
+  // server: {
+  //   proxy: {
+  //     '/graphql': {
+  //       target: 'http://localhost:3000',
+  //     },
+  //     // Only proxy the API endpoint, not the SPA route `/chat/:name`
+  //     '/chat/send': {
+  //       target: 'http://localhost:3000',
+  //     },
+  //   },
+  // },
+
+  ...(mode === 'development'
+    ? {
+        server: {
+          proxy: {
+            '/graphql': {
+              target: 'http://localhost:3000',
+            },
+            '/chat/send': {
+              target: 'http://localhost:3000',
+            },
+          },
+        },
+      }
+    : {}),
+}))
